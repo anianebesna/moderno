dd05b18c5ae8c196db6c0478d7f6de2fa6b14a88
Ania <ania-nebesna@ukr.net>
2020-04-10 22:55:29 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,19 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
 
+    var mixer = mixitup('.products__inner-box');
 
-
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
 
     
 });
