4fbf7eb7cdb984987a57507bd84a790801e1e377
Ania <ania-nebesna@ukr.net>
2020-04-13 01:30:58 +0300

    added button list

--- app/js/main.js
+++ app/js/main.js
@@ -14,8 +14,7 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4,
     });
-
-   
+  
 });
 
 $(".js-range-slider").ionRangeSlider({
@@ -26,3 +25,15 @@ $(".js-range-slider").ionRangeSlider({
     to: 600,
     prefix: "$",
    });
+
+$('.icon-th-list').on('click', function () {
+    $('.product__item').addClass('list'),
+    $('.icon-th-list').addClass('active'),
+    $('.icon-th-large').removeClass('active')
+});
+
+$('.icon-th-large').on('click', function () {
+    $('.product__item').removeClass('list'),
+    $('.icon-th-large').addClass('active'),
+    $('.icon-th-list').removeClass('active')
+});
